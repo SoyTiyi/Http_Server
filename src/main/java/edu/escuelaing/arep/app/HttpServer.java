ef37b7bc75a5117796b95915d0917b2b84be1588
SoyTiyi <santiago.marmartinez@gmail.com>
2021-02-05 13:42:59 -0500

    fix http server

--- src/main/java/edu/escuelaing/arep/app/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/app/HttpServer.java
@@ -1,13 +1,16 @@
 package edu.escuelaing.arep.app;
 
 import java.net.*;
+
+import org.ietf.jgss.GSSException;
+
 import java.io.*;
 
 public class HttpServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(36000);
+            serverSocket = new ServerSocket(getPort());
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
@@ -39,7 +42,14 @@ public class HttpServer {
             out.close();
             in.close();
             clientSocket.close();
-            serverSocket.close();
         }
+        serverSocket.close();
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 36000;
     }
 }
